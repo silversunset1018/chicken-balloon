b9cd1fc5b52340dfe19a4cf79c050b4c41719149
dan <dan@duncalf.com>
2015-08-05 16:25:08 -0700

    Updated AppSteroid to SDK 1.0.2
    This version uses the new AppSteroid interface from 1.0.  Also removed
    removed duplicate files that are already part of the SDK.

--- Assets/Plugins/iOS/FASMail.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <MessageUI/MessageUI.h>
-
-@interface FASMail : UIViewController <MFMailComposeViewControllerDelegate>
-
-+ (FASMail*) instance;
-- (IBAction)showEmail:(NSString*) title :(NSString*) message :(NSString*) to;
-
-@end
--- Assets/Plugins/iOS/ImagePickerPlugin.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-
-@interface ImagePickerPlugin : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>;
-
-@end
